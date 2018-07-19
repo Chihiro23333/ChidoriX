71fd945960a46bec79b2e55151ad3139a87ca2ee
ChihiroX <1154705194@qq.com>
2018-07-19 23:08:02 +0800

    贝塞尔优化划线和贝塞尔波浪效果

--- diyviewcomponent/src/main/java/com/bilibili/diyviewcomponent/view/BezierMoveActionView.java
+++ diyviewcomponent/src/main/java/com/bilibili/diyviewcomponent/view/BezierMoveActionView.java
@@ -1,21 +1,30 @@
 package com.bilibili.diyviewcomponent.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BezierMoveActionView extends View {
 
     private Paint mPaint;
 
+    private Path mPath;
+
+    private float mPreX;
+    private float mPreY;
+
     public BezierMoveActionView(Context context) {
         this(context, null);
     }
 
     public BezierMoveActionView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs, 0);
+        this(context, attrs, 0);
     }
 
     public BezierMoveActionView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -25,6 +34,46 @@ public class BezierMoveActionView extends View {
 
     private void initPaint() {
         mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10);
+        mPaint.setAntiAlias(true);
 
+        mPath = new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+//        Path path = new Path();
+//        path.moveTo(300, 300);
+//        path.quadTo(400, 100, 500, 300);
+//        path.quadTo(600, 500, 700, 300);
+//
+//        canvas.drawPath(path, mPaint);
+
+        canvas.drawPath(mPath, mPaint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(event.getX(), event.getY());
+                mPreX = event.getX();
+                mPreY = event.getY();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float endX = (event.getX() + mPreX) / 2;
+                float endY = (event.getY() + mPreY) / 2;
+                mPath.quadTo(mPreX, mPreY, endX, endY);
+                mPreX = event.getX();
+                mPreY = event.getY();
+                invalidate();
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
---
+++ diyviewcomponent/src/main/java/com/bilibili/diyviewcomponent/view/BezierWaveView.java
@@ -0,0 +1,80 @@
+package com.bilibili.diyviewcomponent.view;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by Chihiro on 2018/7/19.
+ */
+
+public class BezierWaveView extends View {
+
+    private Paint mPaint;
+    private Path mPath;
+    private int waveLegth = 400;
+    private int dx;
+
+    public BezierWaveView(Context context) {
+        this(context, null);
+    }
+
+    public BezierWaveView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BezierWaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10);
+
+        mPath = new Path();
+
+        startAnim();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mPath.reset();
+        mPath.moveTo(-waveLegth + dx, 600);
+
+        for (int i = -waveLegth; i < getWidth() + waveLegth; i += waveLegth) {
+            mPath.rQuadTo(waveLegth / 4, 200, waveLegth/2, 0);
+            mPath.rQuadTo(waveLegth / 4, -200, waveLegth/2, 0);
+        }
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    private void startAnim() {
+        ValueAnimator valueAnimator = ObjectAnimator.ofInt(0, waveLegth);
+        valueAnimator.setDuration(2000);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                dx = (int) valueAnimator.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        valueAnimator.start();
+    }
+}
